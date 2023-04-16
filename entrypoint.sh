1481675aba1b6907593c8cbd1e3d4392f4bb10f0
alitesla <124582352+Aitesla@users.noreply.github.com>
2023-04-16 19:50:42 +0330

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 # 定义 UUID 及伪装路径、哪吒面板参数，请自行修改. (注意:伪装路径以 / 符号开始,为避免不必要的麻烦,请不要使用特殊符号.)
-UUID='de04add9-5c68-8bab-950c-08cd5320df18'
-VMESS_WSPATH='/vmess'
-VLESS_WSPATH='/vless'
-TROJAN_WSPATH='/trojan'
+UUID='1ed82ee6-ffed-41c1-b455-db98d807e09a '
+VMESS_WSPATH='/tesvmess'
+VLESS_WSPATH='/tesvless'
+TROJAN_WSPATH='/testrojan'
 SS_WSPATH='/shadowsocks'
 NEZHA_SERVER=''
 NEZHA_PORT=''
